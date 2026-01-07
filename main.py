38b273497d548aade741f6dd572e8ba01a5fbf75
Arjun Sumesh <arjunsumesh@gmail.com>
2026-01-07 02:16:44 -0500

    Refactored paddle code to seperate class code and creted a second paddle

--- main.py
+++ main.py
@@ -1,27 +1,21 @@
 from turtle import Screen,Turtle
+from paddle import Paddle
 screen= Screen()
 screen.bgcolor("black")
 screen.setup(width=800,height=600)
 screen.title("pong")
 screen.tracer(0)   #To turn the animation off and update it inside the while loop to see the paddle
 
-paddle=Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5,stretch_len=1)
-paddle.penup()
-paddle.goto(350,0)
-
-def go_up():
-    new_y=paddle.ycor()+20    #move 20 to up from the current paddle ycor
-    paddle.goto(paddle.xcor(),new_y)
-def go_down():
-    new_y=paddle.ycor()-20    #move 20 to up from the current paddle ycor
-    paddle.goto(paddle.xcor(),new_y)
+r_paddle=Paddle((350,0))
+l_paddle=Paddle((-350,0))
 
 screen.listen()
-screen.onkey(go_up,"Up")
-screen.onkey(go_down,"Down")
+screen.onkey(r_paddle.go_up,"Up")
+screen.onkey(r_paddle.go_down,"Down")
+
+screen.onkey(l_paddle.go_up,"w")
+screen.onkey(l_paddle.go_down,"s")
+
 game_is_on=True
 while game_is_on:
     screen.update()
