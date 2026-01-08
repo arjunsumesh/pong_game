9065a7aaa57eca90e341d9d84dfef769d7b45b08
Arjun Sumesh <arjunsumesh@gmail.com>
2026-01-08 01:36:00 -0500

    Added ball in to the game and made it move to xy axis

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen,Turtle
 from paddle import Paddle
+from ball import Ball
+import time
 screen= Screen()
 screen.bgcolor("black")
 screen.setup(width=800,height=600)
@@ -8,16 +10,18 @@ screen.tracer(0)   #To turn the animation off and update it inside the while loo
 
 r_paddle=Paddle((350,0))
 l_paddle=Paddle((-350,0))
+ball=Ball()
 
 screen.listen()
 screen.onkey(r_paddle.go_up,"Up")
 screen.onkey(r_paddle.go_down,"Down")
-
 screen.onkey(l_paddle.go_up,"w")
 screen.onkey(l_paddle.go_down,"s")
 
 game_is_on=True
 while game_is_on:
+    time.sleep(0.1)   #Making the while loop sleep for 0.1 seconds
     screen.update()
+    ball.move()
 
 screen.exitonclick()
